c6b0072c0b7d60214d04ed05a10a0d997d0cd8a7
Rifazul <rifazul50@gmail.com>
2023-09-12 06:05:53 +0600

    Insert the Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import Profile from '../../assets/images/profile.png'
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center p-2 m-7 border-b-2">
+       <h1 className="text-3xl font-bold" >Knowledge Cafe</h1>
+       <img src={Profile} alt="" />
+
+    </div>
+    
+  )
+}
+
+Header.propTypes = {
+ Profile: PropTypes.div
+}
+
+export default Header
